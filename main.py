eed8d7e8cc282699e638d1c14314aac5db35e39f
Surya Gunukula <suryagunukula@169-231-143-207.wireless.ucsb.edu>
2025-05-04 18:38:58 -0700

    got pipeline working, have to debug the neo4j database to add relationship descriptions will do tmrw

--- main.py
+++ main.py
@@ -1,8 +1,116 @@
-from llama_index.llms.ollama import Ollama
+import pandas as pd
+from llama_index.core import Document 
 
-def main():
-    print("Hello from graphrag!")
+from llama_index.llms.ollama import Ollama
+from llama_index.core.settings import Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+
+from extractor import *
+from query import *
+from store import * 
+
+import os 
+from llama_index.llms.openai import OpenAI
+from dotenv import load_dotenv
+
+from llama_index.graph_stores.neo4j import Neo4jPropertyGraphStore 
+from llama_index.core import PropertyGraphIndex
+from llama_index.core.node_parser import SentenceSplitter
+
+
+
+
 
 
 if __name__ == "__main__":
-    main()
+    news = pd.read_csv(
+    "https://raw.githubusercontent.com/tomasonjo/blog-datasets/main/news_articles.csv"
+    )[:2]
+    news.head()
+
+    documents = [
+        Document(text=f"{row['title']}: {row['text']}")
+        for i, row in news.iterrows()
+    ]
+    print(documents[0])
+
+    """
+    load_dotenv()
+    api_key = os.getenv("OPENAI_API_KEY")
+
+    llm = OpenAI(model = "gpt4", api_key=api_key)
+    Settings.llm = OpenAI(api_key=api_key, model="gpt-4")
+    Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+    """
+
+    llm = Ollama(
+    model = "llama3",
+    base_url = "http://localhost:11434",
+    request_timeout = 120.0
+    )
+    Settings.llm = llm
+    Settings.embed_model = HuggingFaceEmbedding(model_name = "BAAI/bge-base-en-v1.5")
+
+    # Set up extractor
+    extractor = GraphRAGExtractor(
+        llm=Settings.llm,
+        extract_prompt=KG_TRIPLET_EXTRACT_TMPL,
+        parse_fn=parse_fn,
+        max_paths_per_chunk=5,
+    )
+
+    # Run extraction
+    processed_docs = extractor(documents)
+
+    print(processed_docs[0].metadata.get(KG_NODES_KEY))
+
+    graph_store = GraphRAGStore(
+        username="neo4j", password="S00stest!", url="bolt://localhost:7687"
+    )
+
+    # Print extracted metadata
+    for doc in processed_docs:
+        print(f"\nDoc ID: {doc.id_}")
+        print(f"Text: {doc.text[:300]}...")  # truncate for readability
+        print("📌 Entities:")
+        for node in doc.metadata.get(KG_NODES_KEY, []):
+            print(f"  - {node.name} ({node.label}) - {node.properties.get('entity_description')}")
+        print("🔗 Relationships:")
+        for rel in doc.metadata.get(KG_RELATIONS_KEY, []):
+            print(f"  - {rel.source_id} --[{rel.label}]--> {rel.target_id}")
+            print(f"    Explanation: {rel.properties.get('relationship_description')}")
+        print("=" * 60)
+
+    splitter = SentenceSplitter(
+        chunk_size = 1024,
+        chunk_overlap = 20,
+    )
+
+    nodes = splitter.get_nodes_from_documents(documents)
+
+    source = EntityNode(name="TestSource", label="Company", properties={"id": "TestSourceID"})
+    target = EntityNode(name="TestTarget", label="Market", properties={"id": "TestTargetID"})
+    relation = Relation(
+        label="listed_on",
+        source_id="TestSourceID",
+        target_id="TestTargetID",
+        properties={
+            "relationship_description": "TestSource is listed on TestTarget.",
+            "triplet_source_id": "manual-test-1"
+        }
+    )
+
+
+    index = PropertyGraphIndex(
+        nodes=nodes, 
+        kg_extractor=[extractor],
+        property_graph_store = graph_store,
+        show_progress = False
+    )
+
+    print(index.property_graph_store.get_triplets()[10])
+
+    print(index.property_graph_store.get_triplets()[10][0].properties)
+
+    
+
